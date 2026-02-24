89b493fb5d2c5c8af520dde8213c932fc661ab84
mdmasrafimahbubnirob <mdmasrafimahbubnirob@gmail.com>
2026-02-24 19:15:28 +0600

    js compieted for total count

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+let InterviewList=[];
+let RejectedList=[];
+
+let Total=document.getElementById('total');
+let Interview=document.getElementById('interview');
+let Rejected=document.getElementById('rejected');
+
+
+let allCardsSection=document.getElementById('allCards');
+
+console.log();
+
+function count(){
+    Total.innerText=allCardsSection.children.length;
+}
+
+count();
