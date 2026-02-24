a3f3b4f81cb327c7c829f582f7b38ea37ab13ae9
mdmasrafimahbubnirob <mdmasrafimahbubnirob@gmail.com>
2026-02-24 20:35:33 +0600

    all interview rejected Button completed

--- script.js
+++ script.js
@@ -2,16 +2,48 @@ let InterviewList=[];
 let RejectedList=[];
 
 let Total=document.getElementById('total');
-let Interview=document.getElementById('interview');
-let Rejected=document.getElementById('rejected');
+let InterviewCount=document.getElementById('interview');
+let RejectedCount=document.getElementById('rejected');
 
 
 let allCardsSection=document.getElementById('allCards');
 
-console.log();
+let mainContainer=document.querySelector('main');
+
+// console.log(mainContainer);
+
+// InterviewList.push({name:'m 1'},{name:'m 1'})
+
+let allButton=document.getElementById('all-Button');
+
+let interviewButton=document.getElementById('interview-Button');
+
+let rejectedButton=document.getElementById('rejected-Button');
+
 
 function count(){
     Total.innerText=allCardsSection.children.length;
+
+    InterviewCount.innerText=InterviewList.length;
+
+    RejectedCount.innerText=RejectedList.length;
 }
 
 count();
+
+function toggle(id){
+    // console.log("okok");
+
+    allButton.classList.remove('bg-blue-600','text-white');
+    interviewButton.classList.remove('bg-blue-600','text-white');
+    rejectedButton.classList.remove('bg-blue-600','text-white');
+
+     allButton.classList.add('bg-white','text-gray-500');
+    interviewButton.classList.add('bg-white','text-gray-500');
+    rejectedButton.classList.add('bg-white','text-gray-500');
+
+    let selected=document.getElementById(id);
+
+    selected.classList.remove('bg-white','text-gray-500');
+    selected.classList.add('bg-blue-600','text-white');
+}
